64201ecabc46fcb450f4f509e23cd1d345ec65b4
Chris <c.uzor@yahoo.com>
2020-11-28 00:26:00 +0100

    Interfaces

---
+++ classes/interfaces.php
@@ -0,0 +1,36 @@
+<?php 
+
+interface Newsletter
+{
+    public function subscribe($email);
+}
+
+class CampaignMonitor implements Newsletter{
+    
+    public function subscribe($email)
+    {
+        die('subscribing with Campaign Monitor');
+    }
+}
+
+class Drip implements Newsletter{
+
+    public function subscribe($email)
+    {
+        die('subscribing with drip');
+    }
+}
+
+class NewsletterSubscriptionController{
+
+    public function store(Newsletter $newsletter){
+
+        $email = 'chris@eexample.com';
+
+        $newsletter->subscribe($email);
+    }
+}
+
+$controller = new NewsletterSubscriptionController();
+
+$controller->store(new Drip());
